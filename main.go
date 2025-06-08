c5bdd34cca02532fc09d2b8fa446e3804f2ab608
LJ van Staden <lawrence@luno.com>
2025-06-08 21:00:51 +0200

    Added additional kes pairs

--- main.go
+++ main.go
@@ -1,9 +1,25 @@
 package main
 
-import (
-	gr "example.com/course/greeting"
-)
+import "fmt"
+
+type person struct {
+	age int 
+	name string
+}
+
+func (p *person) changeUser() {
+	p.age = 20
+	p.name = "Bobby"
+}
 
 func main(){
-	gr.Greet()
+	user := person{35,"Fred"}
+	user.changeUser()
+	showUser(&user)
+}
+
+func showUser(p *person){
+	//p.age = 35
+	//p.name = "Steve"
+	fmt.Println(*p)
 }
